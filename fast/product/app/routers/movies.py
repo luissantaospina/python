c6e142a265d9d2de8719c6c23141c6a61b59048f
Luis Santa <luis@lizit.app>
2023-07-08 15:51:45 -0500

    Add dependencies in apirouter

--- fast/product/app/routers/movies.py
+++ fast/product/app/routers/movies.py
@@ -6,51 +6,53 @@ from ..helpers import oauth_schema
 from ..services import MovieService
 from ..repositories import MovieRepository
 
-router = APIRouter(prefix='/movies', tags=["movies"])
+router = APIRouter(
+    prefix='/movies',
+    tags=["movies"],
+    dependencies=[Depends(oauth_schema)]
+)
 
 
 def validate_movie(function):
     @functools.wraps(function)
-    def wrapper(movie_id: int, token: str):
+    def wrapper(movie_id: int, *args):
         movie = MovieRepository.get(movie_id)
         if not movie:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Movie not found')
 
-        return function(movie_id, token)
+        return function(movie_id, *args)
 
     return wrapper
 
 
 @router.post("", response_model=MovieResponseModel, tags=["movies"])
-async def create_movie(movie: MovieRequestModel, token: str = Depends(oauth_schema)) -> MovieResponseModel:
+async def create_movie(movie: MovieRequestModel) -> MovieResponseModel:
     movie_created = MovieService.create_movie(movie)
     return movie_created
 
 
 @router.get("", response_model=List[MovieResponseModel])
-async def get_movies(page: int = 1, limit: int = 10, token: str = Depends(oauth_schema)) \
-        -> List[MovieResponseModel]:
+async def get_movies(page: int = 1, limit: int = 10) -> List[MovieResponseModel]:
     movies = MovieService.get_movies(page, limit)
     return [movie for movie in movies]
 
 
 @router.get("/{movie_id}", response_model=MovieResponseModel)
 @validate_movie
-def get_movie(movie_id: int = Path(ge=1), token: str = Depends(oauth_schema)) -> MovieResponseModel:
+def get_movie(movie_id: int = Path(ge=1)) -> MovieResponseModel:
     movie = MovieService.get_movie(movie_id)
     return movie
 
 
 # TODO: Validate movie
 @router.put("/{movie_id}", response_model=MovieResponseModel)
-async def update_movie(movie_request: MovieRequestModel, movie_id: int = Path(ge=1),
-                       token: str = Depends(oauth_schema)) -> MovieResponseModel:
+async def update_movie(movie_request: MovieRequestModel, movie_id: int = Path(ge=1)) -> MovieResponseModel:
     movie_updated = MovieService.update_movie(movie_request, movie_id)
     return movie_updated
 
 
 @router.delete("/{movie_id}", response_model=MovieResponseModel)
 @validate_movie
-def delete_movie(movie_id: int = Path(ge=1), token: str = Depends(oauth_schema)) -> MovieResponseModel:
+def delete_movie(movie_id: int = Path(ge=1)) -> MovieResponseModel:
     movie_deleted = MovieService.delete_movie(movie_id)
     return movie_deleted
--- fast/product/app/routers/user_reviews.py
+++ fast/product/app/routers/user_reviews.py
@@ -10,17 +10,21 @@ from ..helpers import get_current_user
 from ..services import UserReviewService
 from ..repositories import UserReviewRepository
 
-router = APIRouter(prefix='/reviews', tags=["reviews"])
+router = APIRouter(
+    prefix='/reviews',
+    tags=["reviews"],
+    dependencies=[Depends(oauth_schema)]
+)
 
 
 def validate_review(function):
     @functools.wraps(function)
-    def wrapper(review_id: int, token: str):
+    def wrapper(review_id: int):
         review = UserReviewRepository.get(review_id)
         if not review:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Review not found')
 
-        return function(review_id, token)
+        return function(review_id)
 
     return wrapper
 
@@ -33,7 +37,7 @@ async def create_review(review: UserReviewRequestModel, user: User = Depends(get
 
 
 @router.get("", response_model=List[UserReviewResponseModel])
-async def get_reviews(page: int = 1, limit: int = 10, token: str = Depends(oauth_schema))\
+async def get_reviews(page: int = 1, limit: int = 10)\
         -> List[UserReviewResponseModel]:
     reviews = UserReviewService.get_reviews(page, limit)
     return [review for review in reviews]
@@ -41,15 +45,15 @@ async def get_reviews(page: int = 1, limit: int = 10, token: str = Depends(oauth
 
 @router.get("/{review_id}", response_model=UserReviewResponseModel)
 @validate_review
-def get_review(review_id: int = Path(ge=1), token: str = Depends(oauth_schema)) -> UserReviewResponseModel:
+def get_review(review_id: int = Path(ge=1)) -> UserReviewResponseModel:
     review = UserReviewService.get_review(review_id)
     return review
 
 
 # TODO: Validate review
 @router.put("/{review_id}", response_model=UserReviewResponseModel)
-async def update_review(review_request: UserReviewRequestPutModel, review_id: int = Path(ge=1),
-                        token: str = Depends(oauth_schema)) -> UserReviewResponseModel:
+async def update_review(review_request: UserReviewRequestPutModel, review_id: int = Path(ge=1))\
+        -> UserReviewResponseModel:
     review_updated = UserReviewService.update_review(review_request, review_id)
     return review_updated
 
@@ -57,6 +61,6 @@ async def update_review(review_request: UserReviewRequestPutModel, review_id: in
 # with Path validate the route params
 @router.delete("/{review_id}", response_model=UserReviewResponseModel)
 @validate_review
-def delete_review(review_id: int = Path(ge=1), token: str = Depends(oauth_schema)) -> UserReviewResponseModel:
+def delete_review(review_id: int = Path(ge=1)) -> UserReviewResponseModel:
     review_deleted = UserReviewService.delete_review(review_id)
     return review_deleted
